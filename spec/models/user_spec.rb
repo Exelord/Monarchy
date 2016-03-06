cf3efdb522707e51d9ce0ae62120c9da611bd638
Maciej Kwaśniak <kmaciek17@gmail.com>
2016-03-06 12:47:13 +0100

    Add missing tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,50 +13,51 @@ describe User, type: :model do
   describe '#role_for' do
     let!(:memo_member) { create(:member, user: user, hierarchy: memo.hierarchy, roles: [member_role]) }
     let!(:project_member) { create(:member, user: user, hierarchy: project.hierarchy, roles: [manager_role]) }
+    subject { user.role_for(memo) }
 
     context 'user has no direct access to memo' do
       let!(:memo_member) {}
 
-      it { expect(user.role_for(memo)).to eq(manager_role) }
+      it { is_expected.to eq(manager_role) }
     end
 
     context 'user has no direct access to project' do
       let!(:project_member) {}
 
-      it { expect(user.role_for(memo)).to eq(member_role) }
+      it { is_expected.to eq(member_role) }
     end
 
     context 'parent role is inherited' do
-      it { expect(user.role_for(memo)).to eq(manager_role) }
+      it { is_expected.to eq(manager_role) }
 
       context 'model role is inherited' do
         let(:member_role) { create(:role, name: :member, level: 1) }
 
-        it { expect(user.role_for(memo)).to eq(manager_role) }
+        it { is_expected.to eq(manager_role) }
       end
 
       context 'model role is not inherited' do
         let(:member_role) { create(:role, name: :member, level: 1, inherited: false) }
 
-        it { expect(user.role_for(memo)).to eq(manager_role) }
+        it { is_expected.to eq(manager_role) }
       end
     end
 
     context 'parent role in not inherited' do
       let(:manager_role) { create(:role, name: :manager, level: 2, inherited: false) }
 
-      it { expect(user.role_for(memo)).to eq(member_role) }
+      it { is_expected.to eq(member_role) }
 
       context 'model role is not inherited' do
         let(:member_role) { create(:role, name: :member, level: 1, inherited: false) }
 
-        it { expect(user.role_for(memo)).to eq(member_role) }
+        it { is_expected.to eq(member_role) }
       end
 
       context 'model role is inherited' do
         let(:member_role) { create(:role, name: :member, level: 1) }
 
-        it { expect(user.role_for(memo)).to eq(member_role) }
+        it { is_expected.to eq(member_role) }
       end
     end
   end
@@ -65,23 +66,25 @@ describe User, type: :model do
     let(:default_role) { Role.find_by_name(Treelify.configuration.default_role.name) }
 
     context 'inherited resource' do
-      subject { user.grant(:manager, memo) }
+      let(:grant_user) { user.grant(:manager, memo) }
 
-      it { expect{subject}.to change{Member.count}.by(2) }
+      before do
+        grant_user
+      end
+
+      it { expect(Member.count).to be(2) }
 
       it 'project with default role' do
-        subject
         expect(project.members.first.roles).to match_array([default_role])
       end
 
       it 'memo with selected role' do
-        subject
         expect(memo.members.first.roles).to match_array([Role.find_by_name(:manager), default_role])
       end
 
       it 'with doubled grant' do
         2.times do
-          subject
+          grant_user
           expect(project.members.first.roles).to match_array([default_role])
           expect(memo.members.first.roles).to match_array([Role.find_by_name(:manager), default_role])
         end
@@ -89,10 +92,14 @@ describe User, type: :model do
     end
 
     context 'root resource' do
-      subject { user.grant(:manager, project) }
+      let(:grant_user) { user.grant(:manager, project) }
 
-      it { expect{subject}.to change{Member.count}.by(1) }
-      it { subject; expect(project.members.first.roles).to match_array([Role.find_by_name(:manager), default_role]) }
+      before do
+        grant_user
+      end
+
+      it { expect(Member.count).to be(1) }
+      it { expect(project.members.first.roles).to match_array([Role.find_by_name(:manager), default_role]) }
     end
   end
 
