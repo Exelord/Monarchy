c5782e52e28748edb2997f497c7fb6a86d1b7ffd
Maciej Kwaśniak <kmaciek17@gmail.com>
2016-08-16 10:38:18 +0200

    MOve tasks to Monarchy methods

---
+++ lib/monarchy/tasks.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+require 'tqdm'
+
+module Monarchy
+  class << self
+    def rebuild!
+      p 'Rebuilding Monarchy...'
+      hierarhization
+      reparentize
+      p 'Rebuilding Monarchy ended!'
+    end
+
+    private
+
+    def hierarhization
+      p 'Monarchy hierarchization...'
+      Monarchy.resource_classes.with_progress.each do |klass|
+        klass.all.each do |model|
+          model.send(:ensure_hierarchy)
+        end
+      end
+      p 'Monarchy hierarchization ended!'
+    end
+
+    def reparentize
+      p 'Monarchy reparentize...'
+
+      Monarchy.resource_classes.with_progress.each do |klass|
+        klass.all.each do |model|
+          model.send(:assign_parent, true)
+        end
+      end
+
+      Monarchy::Hierarchy.rebuild!
+      p 'Monarchy reparentize ended!'
+    end
+  end
+end
