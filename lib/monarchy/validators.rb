f360b2fc047048a4f43eb6f1b4dfe9c6754e9358
Maciej Kwaśniak <kmaciek17@gmail.com>
2016-08-21 17:01:59 +0200

    Improve validators

--- lib/monarchy/validators.rb
+++ lib/monarchy/validators.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 module Monarchy
   module Validators
     class << self
@@ -6,11 +7,22 @@ module Monarchy
         role || raise(Monarchy::Exceptions::RoleNotExist, role_name)
       end
 
-      def resource(resource)
-        raise Monarchy::Exceptions::ResourceIsNil unless resource
+      def resource(resource, allow_nil = false)
+        raise Monarchy::Exceptions::ResourceIsNil if !resource && !allow_nil
 
-        true_resource = resource.class.try(:acting_as_resource)
-        raise Monarchy::Exceptions::ModelNotResource, resource unless true_resource
+        if resource
+          true_resource = resource.class.try(:acting_as_resource)
+          raise Monarchy::Exceptions::ModelNotResource, resource unless true_resource
+        end
+      end
+
+      def user(user, allow_nil = false)
+        raise Monarchy::Exceptions::UserIsNil if !user && !allow_nil
+
+        if user
+          true_user = user.is_a?(Monarchy.user_class)
+          raise Monarchy::Exceptions::ModelNotUser, user unless true_user
+        end
       end
     end
   end
