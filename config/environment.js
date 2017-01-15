f5768e7df2c314c3268bb99f3fd21263b9c05e24
Maciej Kwaśniak <kmaciek17@gmail.com>
2017-01-15 17:30:52 +0100

    Fix ember config

--- config/environment.js
+++ config/environment.js
@@ -43,9 +43,7 @@ module.exports = function(environment) {
   }
 
   if (environment === 'production') {
-    ENV.locationType = 'hash';
-    ENV.rootURL = '/monarchy/';
-
+    ENV.rootURL = '/Monarchy/';
   }
 
   return ENV;
