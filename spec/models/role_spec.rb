9ab2e1908484c7bfc6c1bd17b74f00d7127441d8
Maciej Kwaśniak <kmaciek17@gmail.com>
2018-03-24 13:17:55 +0100

    Validate name and role level

--- spec/models/role_spec.rb
+++ spec/models/role_spec.rb
@@ -5,4 +5,7 @@ require 'rails_helper'
 describe Monarchy::Role, type: :model do
   it { is_expected.to have_many(:members).through(:members_roles) }
   it { is_expected.to have_many(:members_roles).dependent(:destroy) }
+
+  it { is_expected.to validate_presence_of(:name) }
+  it { is_expected.to validate_presence_of(:level) }
 end
